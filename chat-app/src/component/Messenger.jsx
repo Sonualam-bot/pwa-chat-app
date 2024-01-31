f3853bfcf6932c21c12225be45884f7e53c90567
Sonualam <sonualam1599@gmail.com>
2024-01-31 21:13:37 +0530

    assembled chat home page componets and login component

--- chat-app/src/component/Messenger.jsx
+++ chat-app/src/component/Messenger.jsx
@@ -2,6 +2,9 @@ import { AppBar, Box, Toolbar, styled } from "@mui/material";
 
 //components
 import LoginDialog from "./account/LoginDialog";
+import ChatDialog from "./chat/ChatDialog";
+import { useContext } from "react";
+import { AccountContext } from "../context/AccountProvider";
 
 const Container = styled(Box)`
   height: 100vh;
@@ -9,18 +12,36 @@ const Container = styled(Box)`
 `;
 
 const Header = styled(AppBar)`
+  height: 125px;
+  background-color: #00a884;
+  box-shadow: none;
+`;
+
+const LoginHeader = styled(AppBar)`
   height: 220px;
   background-color: #00bfa5;
   box-shadow: none;
 `;
 
 function Messenger() {
+  const { account } = useContext(AccountContext);
   return (
     <Container>
-      <Header>
-        <Toolbar></Toolbar>
-      </Header>
-      <LoginDialog />
+      {account ? (
+        <>
+          <Header>
+            <Toolbar></Toolbar>
+          </Header>
+          <ChatDialog />
+        </>
+      ) : (
+        <>
+          <LoginHeader>
+            <Toolbar></Toolbar>
+          </LoginHeader>
+          <LoginDialog />
+        </>
+      )}
     </Container>
   );
 }
