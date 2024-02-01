c5ad6fa3f1f356fbee5f7dab60939b0ec65d75e7
Sonualam <sonualam1599@gmail.com>
2024-02-01 14:15:15 +0530

    added new conversation api and integrated with backend

---
+++ server/controllers/conversation.controller.js
@@ -0,0 +1,34 @@
+import Conversation from "../model/conversation.model.js";
+
+export const newConversation = async (req, res) => {
+  try {
+    const senderId = req.body.senderId;
+    const receiverId = req.body.receiverId;
+
+    const existingConversation = await Conversation.findOne({
+      members: { $all: [receiverId, senderId] },
+    });
+
+    if (existingConversation) {
+      return res.status(200).json({
+        success: true,
+        message: "Conversation already exists",
+      });
+    }
+
+    const newConver = new Conversation({
+      members: [senderId, receiverId],
+    });
+
+    await newConver.save();
+    return res.status(200).json({
+      success: true,
+      message: "Conversation saved successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: true,
+      message: `${error.message}`,
+    });
+  }
+};
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,8 +2,10 @@ import express from "express";
 const router = express.Router();
 
 import { addUser, getUsers } from "../controllers/user.controller.js";
+import { newConversation } from "../controllers/conversation.controller.js";
 
 router.post("/add", addUser);
 router.get("/users", getUsers);
+router.post("/conversation/add", newConversation);
 
 export default router;
