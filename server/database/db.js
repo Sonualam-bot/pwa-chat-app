aa8d0e4bb9b6cd5aa606621da2cf541941e1d49b
Sonualam <sonualam1599@gmail.com>
2024-02-02 22:09:43 +0530

    refactor

--- server/database/db.js
+++ server/database/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const databaseConnection = async () => {
   try {
-    await mongoose.connect(process.env.CONNECTION_URL, {
-      dbName: "whatsApp--clone",
-    });
+    await mongoose.connect(process.env.CONNECTION_URL);
     console.log(`Database connected successfully...`);
   } catch (error) {
     console.log(`Error while connecting with database ${error.message} `);
