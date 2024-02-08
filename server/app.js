d50d98b8a8392c11d742ec16b031e028c2523308
Sonualam <sonualam1599@gmail.com>
2024-02-08 15:33:39 +0530

    changed server config

--- server/app.js
+++ server/app.js
@@ -3,17 +3,25 @@ import dotenv from "dotenv";
 import cors from "cors";
 import bodyParser from "body-parser";
 import { Server } from "socket.io";
+import { createServer } from "http";
 
 import databaseConnection from "./database/db.js";
 import userRouter from "./routes/routes.js";
 
 const app = express();
+const server = createServer(app);
 dotenv.config();
 databaseConnection();
 app.use(express.json());
-app.use(cors({ origin: "*" }));
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Chat server started");
@@ -21,13 +29,11 @@ app.get("/", (req, res) => {
 
 app.use("/", userRouter);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
-
-const io = new Server(process.env.SOCKET_PORT, {
+const io = new Server(server, {
   cors: {
     origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true,
   },
 });
 
@@ -57,4 +63,8 @@ io.on("connection", (socket) => {
 
     io.to(user.socketId).emit("getMessage", data);
   });
+});
+
+server.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
